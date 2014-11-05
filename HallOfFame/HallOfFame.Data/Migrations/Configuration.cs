7612ccd03f6010b3c7fc3cecabb80b591154f22a
StefanSinapov <stefan.sinapov@gmail.com>
2014-11-05 02:19:01 +0200

    Categories seed

--- HallOfFame/HallOfFame.Data/Migrations/Configuration.cs
+++ HallOfFame/HallOfFame.Data/Migrations/Configuration.cs
@@ -1,6 +1,10 @@
 namespace HallOfFame.Data.Migrations
 {
+    using System.Collections.Generic;
     using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    using HallOfFame.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<HallOfFameDbContext>
     {
@@ -12,17 +16,29 @@ namespace HallOfFame.Data.Migrations
 
         protected override void Seed(HallOfFameDbContext context)
         {
-            // This method will be called after migrating to the latest version.
+            this.SeedCategories(context);
+        }
 
-            // You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            // to avoid creating duplicate seed data. E.g.
-            // 
-            // context.People.AddOrUpdate(
-            // p => p.FullName,
-            // new Person { FullName = "Andrew Peters" },
-            // new Person { FullName = "Brice Lambson" },
-            // new Person { FullName = "Rowan Miller" }
-            // );
+        private void SeedCategories(HallOfFameDbContext context)
+        {
+            if (context.Categories.Any())
+            {
+                return;
+            }
+
+            var categories = new HashSet<Category>
+                                 {
+                                     new Category { Name = "Programming with C#", Description = "Курсовете от това направление ще ви дадат основни познания по програмиране и езика C#, писане на качествен код, обектно-ориентирано програмиране, структури от данни и алгоритми, бази данни. Те ще ви дадат необходимите знания за силен старт в сферата на разработка на софтуер." },
+                                     new Category { Name = "Software Technologies", Description = "В това направление ще получите специализирани знания в области като операционни системи, Windows и Linux системна администрация, уеб услуги, мобилни приложения, разработка на софтуер в облачна среда. " },
+                                     new Category { Name = "Web Design & Development", Description = "Обученията по уеб дизайн и UI технологии ще ви научат да създавате уебсайтове с HTML 5, CSS 3, JavaScript и XAML. Ще се запознаете и с най-популярните системи за управление на съдържание като WordPress, Joomla!, Drupal и Sitefinity." },
+                                     new Category { Name = "Quality Assurance", Description = "Курсовете ще ви запознаят с основните практики и техники, използвани в процеса по осигуряване на качеството и автоматизираното тестване на софтуер. Обучението е подходящо за хора с базови познания по програмиране." },
+                                     new Category { Name = "Soft Skills and Business Skills", Description = "В основата на обученията от това направление е развитието на комуникативни и бизнес умения, които надграждат придобитите вече технически знания. Курсовете са с практическа насоченост." }
+                                 };
+
+            foreach (var category in categories)
+            {
+                context.Categories.AddOrUpdate(category);
+            }
         }
     }
 }
