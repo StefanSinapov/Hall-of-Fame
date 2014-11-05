9db9ce5b570d76a2f7d82f4777bed3b5b8bf6ee5
StefanSinapov <stefan.sinapov@gmail.com>
2014-11-05 12:32:08 +0200

    Roles seed added

--- HallOfFame/HallOfFame.Data/HallOfFameDbContext.cs
+++ HallOfFame/HallOfFame.Data/HallOfFameDbContext.cs
@@ -31,6 +31,5 @@ namespace HallOfFame.Data
         {
             return new HallOfFameDbContext();
         }
-      
     }
 }
--- HallOfFame/HallOfFame.Data/Migrations/Configuration.cs
+++ HallOfFame/HallOfFame.Data/Migrations/Configuration.cs
@@ -4,7 +4,12 @@ namespace HallOfFame.Data.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
+    using HallOfFame.Common;
+    using HallOfFame.Data.Contracts;
     using HallOfFame.Models;
+
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     internal sealed class Configuration : DbMigrationsConfiguration<HallOfFameDbContext>
     {
@@ -16,10 +21,68 @@ namespace HallOfFame.Data.Migrations
 
         protected override void Seed(HallOfFameDbContext context)
         {
+
+            context.Configuration.LazyLoadingEnabled = true;
+
+            this.SeedRoles(context);
             this.SeedCategories(context);
         }
 
-        private void SeedCategories(HallOfFameDbContext context)
+        private void SeedRoles(HallOfFameDbContext context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<User>(new UserStore<User>(context));
+
+            // Admin and Administrator Role
+            if (!roleManager.RoleExists(GlobalConstants.AdministratorRoleName))
+            {
+                roleManager.Create(new IdentityRole(GlobalConstants.AdministratorRoleName));
+            }
+
+            this.SeedAdminUser(userManager);
+
+            // Moderator and Moderator Role
+            if (!roleManager.RoleExists(GlobalConstants.ModeratorRoleName))
+            {
+                roleManager.Create(new IdentityRole(GlobalConstants.ModeratorRoleName));
+            }
+
+            this.SeedModeratorUser(userManager);
+        }
+
+        private void SeedModeratorUser(UserManager<User> userManager)
+        {
+           /* var userModerator = new User { UserName = GlobalConstants.ModeratorUserName };
+            userModerator.Email = userModerator.UserName;
+
+            if (userManager.FindByName(GlobalConstants.ModeratorUserName) == null)
+            {
+                var result = userManager.Create(userModerator, GlobalConstants.ModeratorPassword);
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRole(userModerator.Id, GlobalConstants.ModeratorRoleName);
+                }
+            }*/
+        }
+
+        private void SeedAdminUser(UserManager<User> userManager)
+        {
+            /*var user = new User { UserName = GlobalConstants.AdministratorUserName };
+            user.Email = user.UserName;
+
+            if (userManager.FindByName(GlobalConstants.AdministratorUserName) == null)
+            {
+                var result = userManager.Create(user, GlobalConstants.AdministratorPassword);
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, GlobalConstants.AdministratorRoleName);
+                }
+            }*/
+        }
+
+        private void SeedCategories(IHallOfFameDbContext context)
         {
             if (context.Categories.Any())
             {
@@ -39,6 +102,16 @@ namespace HallOfFame.Data.Migrations
             {
                 context.Categories.AddOrUpdate(category);
             }
+        }
+
+        private void SeedCourses(IHallOfFameDbContext context)
+        {
+            if (context.Courses.Any())
+            {
+                return;
+            }
+
+            // var courses = new HashSet<Course>();
         }
     }
 }
