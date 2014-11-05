461ae898796c42f73985ff6fa68dd5e0dc34e21e
StefanSinapov <stefan.sinapov@gmail.com>
2014-11-05 12:32:43 +0200

    global constant ( defautl user avatar url) added

--- HallOfFame/HallOfFame.Common/GlobalConstants.cs
+++ HallOfFame/HallOfFame.Common/GlobalConstants.cs
@@ -5,5 +5,8 @@
         public const string AdministratorRoleName = "Administrator";
 
         public const string ModeratorRoleName = "Moderator";
+
+        public const string DefaultAvatarUrl =
+            "http://bs1.cdn.telerik.com/v1/7syJVKUQCuKgrzVP/066b5760-63a1-11e4-8638-39d391c80554";
     }
 }
