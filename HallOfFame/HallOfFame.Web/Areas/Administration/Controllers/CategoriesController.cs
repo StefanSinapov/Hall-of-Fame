3e870be136c36cdff97d141474e0aca205d5bac0
StefanSinapov <stefan.sinapov@gmail.com>
2014-11-06 15:16:08 +0200

    cleanup  - Administration/Categories

--- HallOfFame/HallOfFame.Web/Areas/Administration/Controllers/CategoriesController.cs
+++ HallOfFame/HallOfFame.Web/Areas/Administration/Controllers/CategoriesController.cs
@@ -1,18 +1,14 @@
 ﻿namespace HallOfFame.Web.Areas.Administration.Controllers
 {
-    using System;
-    using System.Linq;
     using System.Web.Mvc;
 
     using HallOfFame.Data;
     using HallOfFame.Data.Contracts;
-    using HallOfFame.Web.Areas.Administration.Models.Categories;
     using HallOfFame.Web.Controllers;
 
     public class CategoriesController : AdministrationController
     {
-        private const int DefaultPageSize = 1;
-
+        // TODO: Remove after ninject
         public CategoriesController()
             : this(new HallOfFameData(new HallOfFameDbContext()))
         {
@@ -23,39 +19,9 @@
         {
         }
 
-        public ActionResult Index(string searchFilter, int? page, string currentFilter, string sorting = "id")
+        public ActionResult Index()
         {
-            ViewBag.CurrentSort = sorting;
-
-            ViewBag.IdSort = sorting == "Id" ? "Id descending" : "Id";
-            ViewBag.NameSort = sorting == "Name" ? "Name descending" : "Name";
-            ViewBag.CoursesCountSort = sorting == "CoursesCount" ? "CoursesCount descending" : "CoursesCount";
-
-            if (searchFilter != null)
-            {
-                page = 1;
-            }
-            else
-            {
-                searchFilter = currentFilter;
-            }
-
-            ViewBag.SearchFilter = searchFilter;
-
-            var selectedItems = this.Data.Categories.All();
-
-            if (!String.IsNullOrEmpty(searchFilter))
-            {
-                selectedItems = selectedItems
-                   .Where(x => x.Name.ToLower().Contains(searchFilter.ToLower()));
-            }
-
-            var sortedItems = selectedItems.Select(ListCategoryViewModel.ViewModel);
-
-            int pageSize = 1;
-            int pageNumber = (page ?? 1);
-
-            return View();
+            return this.View();
         }
     }
 }
