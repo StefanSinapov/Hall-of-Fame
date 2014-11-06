81b73fb06474d232a2472c95010eebe8700ec2a4
StefanSinapov <stefan.sinapov@gmail.com>
2014-11-06 02:58:20 +0200

    BaseController and AdministrationController added

---
+++ HallOfFame/HallOfFame.Web/Controllers/AdministrationController.cs
@@ -0,0 +1,16 @@
+﻿namespace HallOfFame.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using HallOfFame.Common;
+    using HallOfFame.Data.Contracts;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class AdministrationController : BaseController
+    {
+        public AdministrationController(IHallOfFameData data)
+            : base(data)
+        {
+        }
+    }
+}
---
+++ HallOfFame/HallOfFame.Web/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿namespace HallOfFame.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using HallOfFame.Data.Contracts;
+
+    public class BaseController : Controller
+    {
+        public BaseController(IHallOfFameData provider)
+        {
+            this.Data = provider;
+        }
+
+        public IHallOfFameData Data { get; set; }
+    }
+}
