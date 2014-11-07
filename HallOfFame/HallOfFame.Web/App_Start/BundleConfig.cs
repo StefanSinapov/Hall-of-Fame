24a9bbb11118e1cb3ca07ea018a253e3d052231c
StefanSinapov <stefan.sinapov@gmail.com>
2014-11-07 17:12:29 +0200

    Kendo styles and scripts added to layout and bundleconfig

--- HallOfFame/HallOfFame.Web/App_Start/BundleConfig.cs
+++ HallOfFame/HallOfFame.Web/App_Start/BundleConfig.cs
@@ -9,9 +9,11 @@
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
@@ -23,7 +25,17 @@
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                        "~/Scripts/kendo/2014.2.716/kendo.web.min.js"));
+                        /*"~/Scripts/kendo/2014.2.716/kendo.aspnetmvc.js"*/
+
+           /* bundles.Add(new StyleBundle("~/Content/KendoUI/kendo").Include(
+                      "~/Content/KendoUI/kendo.common.min.css",
+                      "~/Content/KendoUI/kendo.metro.min.css"));*/
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/kendo/2014.2.716/kendo.common.min.css",
+                      "~/Content/kendo/2014.2.716/kendo.metro.min.css",
                       "~/Content/bootstrap-theme.min.css",
                       "~/Content/font-awesome.css",
                       "~/Content/site.css"));
