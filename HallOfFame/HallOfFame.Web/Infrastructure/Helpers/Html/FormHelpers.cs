10b96bd6b8a4df10cc3a5f0910f00f5fa64e3104
StefanSinapov <stefan.sinapov@gmail.com>
2014-11-07 10:54:55 +0200

    Submit button helper

---
+++ HallOfFame/HallOfFame.Web/Infrastructure/Helpers/Html/FormHelpers.cs
@@ -0,0 +1,17 @@
+﻿namespace HallOfFame.Web.Infrastructure.Helpers.Html
+{
+    using System.Web.Mvc;
+
+    public static class FormHelpers
+    {
+        public static MvcHtmlString Submit(this HtmlHelper helper, string value, object htmlAttributes = null)
+        {
+            var submitButton = new TagBuilder("input");
+            submitButton.AddCssClass("btn btn-default");
+            submitButton.Attributes.Add("type", "submit");
+            submitButton.Attributes.Add("value", value);
+            submitButton.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+            return new MvcHtmlString(submitButton.ToString());
+        } 
+    }
+}
